802d8937b6c42a6d214c2fe06bc36004401575be
Michaël Fortin <fortinmike@irradiated.net>
2019-08-19 23:00:15 -0400

    Header and menu styling + logo

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(config) {
+  // Copy file as-is to the output directory
+  config.addPassthroughCopy("source/images");
+
   return {
     templateFormats: ["html", "md", "css"]
   };
