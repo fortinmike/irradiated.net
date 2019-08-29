b7f86420bcbebb319d56d55ca09b8b895d7fb0b7
Michaël Fortin <fortinmike@irradiated.net>
2019-08-28 21:01:19 -0400

    Add appcasts (passthrough copy)

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { orderByExplicitOrder } = require("./utils/collection-utils");
 
 module.exports = function(config) {
   config.addPassthroughCopy("source/images");
+  config.addPassthroughCopy("source/appcasts");
 
   config.addCollection("app", collection => orderByExplicitOrder(collection.getFilteredByTag("app")));
   config.addCollection("menu", collection => orderByExplicitOrder(collection.getFilteredByTag("menu")));
