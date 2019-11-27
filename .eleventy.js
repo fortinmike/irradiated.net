d24a983f81b26ea3e02658842b5bb598de9f96a9
Michaël Fortin <fortinmike@irradiated.net>
2019-11-27 14:00:27 -0500

    Add Illuminated Clock Screen Saver and Pro Picker (tools menu item)

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const { orderByExplicitOrder } = require("./utils/collection-utils");
 module.exports = function(config) {
   config.addPassthroughCopy("source/images");
   config.addPassthroughCopy("source/appcasts");
+  config.addPassthroughCopy("source/files");
 
   config.addCollection("app", collection => orderByExplicitOrder(collection.getFilteredByTag("app")));
   config.addCollection("menu", collection => orderByExplicitOrder(collection.getFilteredByTag("menu")));
